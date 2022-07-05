c917da6568746baa046caa53065df17c967a5fee
Yillié <yilliee@protonmail.com>
2022-07-05 23:08:23 +0500

    a51: Exclude python prebuilts from twrp
    - The inclusion of python prebuilts causes bootloop on 2020 exynos9611 devices.

--- device.mk
+++ device.mk
@@ -4,5 +4,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd \
-    android.hardware.fastboot@1.0implmock
+    android.hardware.fastboot@1.0-impl-mock
 
+# Python prebuilts
+TW_EXCLUDE_PYTHON := true
