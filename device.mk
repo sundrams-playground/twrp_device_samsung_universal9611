00b9ea513f143576cab31a02b44516fe4a94d004
xyz-sundram <sundrraam@gmail.com>
2024-05-30 18:36:52 +0530

    TEMP: remove dynamic partition flags

--- device.mk
+++ device.mk
@@ -1,15 +1,15 @@
 DEVICE_PATH := device/samsung/universal9611
 
 # Dynamic Partition Flag
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+#PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Lpdump
 TW_EXCLUDE_LPDUMP := true
 
 # fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock
+#PRODUCT_PACKAGES += \
+#    fastbootd \
+#    android.hardware.fastboot@1.0-impl-mock
 
 # Python prebuilts
 TW_EXCLUDE_PYTHON := true
