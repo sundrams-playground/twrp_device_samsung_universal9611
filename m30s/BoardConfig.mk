97e6093610ddb6dad648af1bc9f76761956a5084
xyz-sundram <sundrraam@gmail.com>
2024-06-11 16:18:15 +0530

    m30s: update prebuilt kernel

--- m30s/BoardConfig.mk
+++ m30s/BoardConfig.mk
@@ -16,8 +16,8 @@ DEVICE_PATH := device/samsung/universal9611
 DEVICE := m30s
 
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/$(DEVICE)/prebuilt/recovery_dtbo
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/$(DEVICE)/prebuilt/zImage
-BOARD_KERNEL_IMAGE_NAME := zImage
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/$(DEVICE)/prebuilt/Image
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 1 --board SRPSE30A002RU
 
 # Recovery
